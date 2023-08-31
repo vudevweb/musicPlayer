c455d0c5f41688bf38100194292f699ae1248e55
Vũ Đỗ <vudo.contact.vn@gmail.com>
2023-08-31 15:17:29 +0700

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,66 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [{
+        name: "Harley Bird - Home",
+        artist: "Jordan Schor",
+        img: "music-1.jpg",
+        src: "music-1",
+    },
+    {
+        name: "Ikson Anywhere – Ikson",
+        artist: "Audio Library",
+        img: "music-2.jpg",
+        src: "music-2",
+    },
+    {
+        name: "Beauz & Jvna - Crazy",
+        artist: "Beauz & Jvna",
+        img: "music-3.jpg",
+        src: "music-3",
+    },
+    {
+        name: "Hardwind - Want Me",
+        artist: "Mike Archangelo",
+        img: "music-4.jpg",
+        src: "music-4",
+    },
+    {
+        name: "Jim - Sun Goes Down",
+        artist: "Jim Yosef x Roy",
+        img: "music-5.jpg",
+        src: "music-5",
+    },
+    {
+        name: "Lost Sky - Vision NCS",
+        artist: "NCS Release",
+        img: "music-6.jpg",
+        src: "music-6",
+    },
+    {
+        name: "Chỉ còn những mùa nhớ",
+        artist: "VuxDox REMIX",
+        img: "chi-con-nhung-mua-nho.jpg",
+        src: "chi-con-nhung-mua-nho",
+    },
+    {
+        name: "Nàng (ver nữ)",
+        artist: "Thazh - Đông REMIX",
+        img: "nang-ver-nu.jpg",
+        src: "nang-ver-nu",
+    },
+    {
+        name: "À Lôi!",
+        artist: "Double2T x Masew",
+        img: "a-loi.jpg",
+        src: "a-loi",
+    },
+];
